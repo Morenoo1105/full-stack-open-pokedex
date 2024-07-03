d64b6b619c25e03ae0b4bdc8298484553b341cc1
Pablo Moreno <pablo_moreno_martin@hotmail.com>
2024-07-03 19:43:43 +0200

    bugfixes?

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -1,19 +1,17 @@
-const { test, describe, expect } = require("@playwright/test");
+import { test, expect } from "@playwright/test";
 
-describe("Pokedex", () => {
-  test("front page can be opened", async ({ page }) => {
-    await page.goto("");
-    await expect(page.getByText("ivysaur")).toBeVisible();
-    await expect(
-      page.getByText(
-        "Pokémon and Pokémon character names are trademarks of Nintendo."
-      )
-    ).toBeVisible();
-  });
+test("front page can be opened", async ({ page }) => {
+  await page.goto("");
+  await expect(page.getByText("ivysaur")).toBeVisible();
+  await expect(
+    page.getByText(
+      "Pokémon and Pokémon character names are trademarks of Nintendo."
+    )
+  ).toBeVisible();
+});
 
-  test("ivysaur can be navigated to", async ({ page }) => {
-    await page.goto("");
-    await page.click("text=ivysaur");
-    await expect(page.locator("text=chlorophyll")).toBeVisible();
-  });
+test("ivysaur can be navigated to", async ({ page }) => {
+  await page.goto("");
+  await page.click("text=ivysaur");
+  await expect(page.locator("text=chlorophyll")).toBeVisible();
 });
